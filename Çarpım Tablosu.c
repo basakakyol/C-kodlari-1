385fff85d747b3c879b580addf6e30c777be9b91
basakakyol <69796691+basakakyol@users.noreply.github.com>
2020-12-21 17:53:46 +0300

    Create Çarpım Tablosu.c

---
+++ Çarpım Tablosu.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+  int a,b;
+for(a=1;a<=10;a++){
+for(b=1;b<=10;b++){
+printf(" %d * %d = %d \n",a,b,a*b);
+}
+printf("\n");
+//bu kod 1-10 arasındaki sayıların çarpım tablosudur
+  }
+  return 0;
+}
