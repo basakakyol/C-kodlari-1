4d7cdfa4b93183f79a8f6c8fd85a587e376f44ed
basakakyol <69796691+basakakyol@users.noreply.github.com>
2020-12-22 17:45:44 +0300

    Update İki sayı arasındaki asal sayıları bulma.c

--- İki sayı arasındaki asal sayıları bulma.c
+++ İki sayı arasındaki asal sayıları bulma.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 
 int main(void) {
-  int i,b,min,a,flag;
-      printf("Bir aralık girin\n");
-      printf("Aralığın en küçük sayısı:");
+  int i,b,min,a,d;
+  printf("Bir aralık girin\n");
+  printf("Aralığın en küçük sayısı:");
   scanf("%d", &min);
-      printf("Aralığın en büyük sayısı:");
+  printf("Aralığın en büyük sayısı:");
   scanf("%d", &b);
     
-       printf("\nAsal sayılar %d ve %d arasındakiler: ", min, b);
+     printf("\nAsal sayılar %d ve %d arasındakiler: ", min, b);
  
   for( a= min; a <= b; a++){
     
-    for(i=2;i<=a/2;i++){
-         flag = 1;
+    for(i=2;i<a;i++){
       if(a % i == 0) {
-         flag = 0;
-                break;
+        d=0;
+        break;
       }
+        else{
+     d=1;
+
+   }
   }
-   if (flag == 1)
-        printf(" %d" , a);
+   if(d==1)
+ {
+   printf(" %d" , a);
+ }  
+
   }
   return 0;
   }
